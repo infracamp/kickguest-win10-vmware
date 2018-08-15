e60ed4041f06420cf4ef9092441ddd551f508bce
Matthias Leuffen <matthes@leuffen.de>
2018-08-15 22:33:23 +0200

    update

--- provision/install.sh
+++ provision/install.sh
@@ -4,5 +4,12 @@
 sudo echo "PermitEmptyPasswords yes" >> /etc/ssh/sshd_config
 sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 sudo apt-get install docker.io curl git pwgen whois open-vm-tools
-
 sudo gpasswd -a $USER docker
+
+sudo cp /tmp/kickguest-win10-vmware/provision/rc.local /etc/rc.local
+sudo chmod 755 /etc/rc.local
+
+
+
+
+
