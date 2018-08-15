2803381bdb5847cefb97713083d31163bf8fc451
Matthias Leuffen <matthes@leuffen.de>
2018-08-15 23:22:09 +0200

    motd

--- provision/install.sh
+++ provision/install.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
+## Error if something goes wrong
+set -e
 
+INSTALL_DIR=/tmp/kickguest-win10-vmware/provision
+
+## We want empty passwords for ssh:
 sudo echo "PermitEmptyPasswords yes" >> /etc/ssh/sshd_config
 sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-sudo apt-get install docker.io curl git pwgen whois open-vm-tools
+
+## Install the packages
+sudo apt-get install -y docker.io curl git pwgen whois open-vm-tools
+
+## Allow normal user to access docker command
 sudo gpasswd -a $USER docker
 
-sudo cp /tmp/kickguest-win10-vmware/provision/rc.local /etc/rc.local
-sudo chmod 755 /etc/rc.local
+## Remove the default password from user (login without passwd)
+sudo passwd -d user
+
+## Copy files
+sudo rm /etc/update-motd.d/*
+sudo cp -R $INSTALL_DIR/* /etc/
+sudo chmod -x /etc/rc.local
+sudo chmod -x /etc/update-motd.d/*
 
 
 
+echo "DONE!"
 
 
--- provision/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-
-
-sudo vmhgfs-fuse .host:$(vmware-hgfsclient) /mnt -o allow_other
-
